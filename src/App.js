24000d0796ceac901ecadb2620d52cac3d878d2f
usama aamir <usamaaamirsohail@gmail.com>
2021-12-22 15:35:54 +0500

    done

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./App.css";
+import PayPal from "./components/PayPal";
+
+function App() {
+  const [checkout, setCheckOut] = useState(false);
+
+  return (
+    <div className="App">
+      {checkout ? (
+        <PayPal />
+      ) : (
+        <button 
+          onClick={() => {
+            setCheckOut(true);
+          }}
+        >
+          Checkout
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default App;
